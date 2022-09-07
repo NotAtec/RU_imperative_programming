c2953f434bf975153afc0d3d7c02a270df71b87b
Jeroen B <jeroenb04@live.com>
2022-09-07 14:01:04 +0200

    Solution to Part 2 (Chaos of Balls)

--- wxCharles-Assignment-1/agent.cpp
+++ wxCharles-Assignment-1/agent.cpp
@@ -4,7 +4,7 @@
 void get_available_ball() {
   // Function checks if ball is available and will then pick up said ball
   if(on_ball()) {
-      get_ball();
+    get_ball();
   }
 }
 
@@ -14,7 +14,71 @@ void sweep_line() {
     get_available_ball();
     step();
   }
+
   get_available_ball(); // Pick up one more time, in case there is a ball right in front of the wall
+}
+
+void sweep_until_clear() {
+  // Function will sweep line until ball is not visible on new cell
+  while(!in_front_of_wall() && on_ball()) {
+    get_available_ball();
+    step();
+  }
+}
+
+void move_to_facing_wall() {
+  // Function will continue straight ahead, skipping balls, and move safely to the wall Charles is facing
+  while(!in_front_of_wall()) {
+    step();
+  }
+}
+
+void face_north() {
+  // Function will make Charles face North
+  while(!north()) {
+    turn_left();
+  }
+}
+
+void face_west() {
+  // Function will make Charles face West
+  face_north();
+  turn_left();
+}
+
+void face_south() {
+  // Function will make Charles face South
+  face_north();
+  turn_left();
+  turn_left();
+}
+
+void face_east() {
+  // Function will make Charles face East
+  face_north();
+  turn_right();
+}
+
+void move_down() {
+  // Function will move Charles down one line.
+  face_south();
+  step();
+}
+
+void set_to_next_line() {
+  // Function will move Charles to the start of the next line, facing west.
+  move_down();
+  face_east();
+  move_to_facing_wall();
+}
+
+void reset_to_start() {
+  // Function will reset Charles to the starting position, facing the way he faced at the start.
+  face_north();
+  move_to_facing_wall();
+  face_west();
+  move_to_facing_wall();
+  face_east();
 }
 
 void test_agent() {
@@ -32,7 +96,15 @@ void string_agent() {
 }
 
 void chaos_agent() {
+  move_to_facing_wall(); // Setup to the east of the grid to start sweeping
+  while(on_ball()) { 
+    // Loop will make Charles sweep the line, then set to the next line. On the start it checks if the line will be empty.
+    face_west();
+    sweep_until_clear();
+    set_to_next_line();
+  }
 
+  reset_to_start(); // Reset to starting position to finish the exercise.
 }
 
 void block_agent() {
