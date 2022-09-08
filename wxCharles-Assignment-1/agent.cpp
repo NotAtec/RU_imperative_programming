46ccbc4ad28853cbd52af87062ae88f63c228bab
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2022-09-08 22:31:57 +0200

    Restructure code

--- wxCharles-Assignment-1/agent.cpp
+++ wxCharles-Assignment-1/agent.cpp
@@ -1,93 +1,36 @@
+/******************************************************************************
+** Course: Imperative Programming - Assignment 1
+** Authors: Jeroen Brinkhorst [S1101799]
+**          Andrei Ujica [S1102725]
+** Date: 08.09.2022
+******************************************************************************/
+
 #include "framework/charles.h"
 #include "assignments/assignment1.h"
 
-void get_available_ball() {
-  // Function checks if ball is available and will then pick up said ball
-  if(on_ball()) {
-    get_ball();
-  }
-}
+/******************************************************************************
+** Function Prototypes
+******************************************************************************/
 
-void sweep_line() {
-  // Function will continue straight ahead when safe to do so, and pick up balls along the way
-  while(!in_front_of_wall()) {
-    get_available_ball();
-    step();
-  }
+void face_north();
+void face_west();
+void face_south();
+void face_east();
 
-  get_available_ball(); // Pick up one more time, in case there is a ball right in front of the wall
-}
+void move_to_facing_wall();
+void move_down();
+void set_to_next_line();
+void reset_to_start();
+void find_ball_on_line();
 
-void sweep_until_clear() {
-  // Function will sweep line until ball is not visible on new cell
-  while(!in_front_of_wall() && on_ball()) {
-    get_available_ball();
-    step();
-  }
-}
+void sweep_line();
+void sweep_until_clear();
 
-void move_to_facing_wall() {
-  // Function will continue straight ahead, skipping balls, and move safely to the wall Charles is facing
-  while(!in_front_of_wall()) {
-    step();
-  }
-}
+void get_available_ball();
 
-void face_north() {
-  // Function will make Charles face North
-  while(!north()) {
-    turn_left();
-  }
-}
-
-void face_west() {
-  // Function will make Charles face West
-  face_north();
-  turn_left();
-}
-
-void face_south() {
-  // Function will make Charles face South
-  face_north();
-  turn_left();
-  turn_left();
-}
-
-void face_east() {
-  // Function will make Charles face East
-  face_north();
-  turn_right();
-}
-
-void move_down() {
-  // Function will move Charles down one line.
-  face_south();
-  step();
-}
-
-void set_to_next_line() {
-  // Function will move Charles to the start of the next line, facing west.
-  move_down();
-  face_east();
-  move_to_facing_wall();
-}
-
-void reset_to_start() {
-  // Function will reset Charles to the starting position, facing the way he faced at the start.
-  face_north();
-  move_to_facing_wall();
-  face_west();
-  move_to_facing_wall();
-  face_east();
-}
-
-void find_ball_on_line() {
-  // Function will move Charles along line, until a cell with a ball in it is found.
-  while(!on_ball()) {
-    step();
-  }
-}
-
+/******************************************************************************
+** Exercises
+******************************************************************************/
 void test_agent() {
 
 }
@@ -161,4 +104,98 @@ void block_agent() { // Bonus Assignment
 
   put_ball();
   reset_to_start();
+}
+
+
+/******************************************************************************
+** Function Definitions
+******************************************************************************/
+
+// Functions for setting Charles direction
+void face_north() {
+  while(!north()) {
+    turn_left();
+  }
+}
+
+void face_west() {
+  face_north();
+  turn_left();
+}
+
+void face_south() {
+  face_north();
+  turn_left();
+  turn_left();
+}
+
+void face_east() {
+  face_north();
+  turn_right();
+}
+
+
+// Functions for moving Charles to certain points in space
+void move_to_facing_wall() {
+  // Function will continue straight ahead, skipping balls, and move safely to the wall Charles is facing
+  while(!in_front_of_wall()) {
+    step();
+  }
+}
+
+void move_down() {
+  // Function will move Charles down one line.
+  face_south();
+  step();
+}
+
+void set_to_next_line() {
+  // Function will move Charles to the start of the next line, facing west.
+  move_down();
+  face_east();
+  move_to_facing_wall();
+}
+
+void reset_to_start() {
+  // Function will reset Charles to the starting position, facing the way he faced at the start.
+  face_north();
+  move_to_facing_wall();
+  face_west();
+  move_to_facing_wall();
+  face_east();
+}
+
+void find_ball_on_line() {
+  // Function will move Charles along line, until a cell with a ball in it is found.
+  while(!on_ball()) {
+    step();
+  }
+}
+
+
+// Functions for sweeping a line and clearing all balls on it
+void sweep_line() {
+  // Function will continue straight ahead when safe to do so, and pick up balls along the way
+  while(!in_front_of_wall()) {
+    get_available_ball();
+    step();
+  }
+
+  get_available_ball(); // Pick up one more time, in case there is a ball right in front of the wall
+}
+
+void sweep_until_clear() {
+  // Function will sweep line until ball is not visible on new cell
+  while(!in_front_of_wall() && on_ball()) {
+    get_available_ball();
+    step();
+  }
+}
+
+
+// Function for safely getting ball if one is available
+void get_available_ball() {
+  if(on_ball()) {
+    get_ball();
+  }
 }
