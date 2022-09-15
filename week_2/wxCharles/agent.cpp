8bfe735aad8b39ed22e8b4f977688759f505bae2
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2022-09-15 19:30:07 +0200

    Unclean solution part 1

--- week_2/wxCharles/agent.cpp
+++ week_2/wxCharles/agent.cpp
@@ -19,6 +19,13 @@ void line_of_balls();
 void beginning_of_line();
 void setup_next_part();
 
+void follow_line();
+void known_position();
+void find_next_line();
+void check_right();
+void face_north();
+void face_east();
+
 /******************************************************************************
 ** Exercises
 ******************************************************************************/
@@ -28,7 +35,17 @@ void test_agent() {
 
 // Exercise 1 - Follow contiguous path of balls
 void path_agent() {
-  
+  // For Exercise 1 i have made one assumption: Walls only exist on the edges of the world,
+  // As such, there will never exist a situation where there is a wall on the left whilst there is also a wall on the right of Charles.
+
+  while(on_ball()) {
+    follow_line();
+    find_next_line();
+  }
+
+  turn_times(2);
+  step();
+  face_east();
 }
 
 // Exercise 2 - Create Stalactites & Stalagmites
@@ -100,5 +117,61 @@ void beginning_of_line() {
 void setup_next_part() {
   turn_right();
   walk_to_wall();
+  turn_right();
+}
+
+// Part 1 functions, unsorted
+void follow_line() {
+  while(on_ball() & !in_front_of_wall()) {
+    step();
+  }
+
+  if(!on_ball()) {
+    known_position();
+  }
+}
+
+void known_position() {
+  turn_times(2);
+  step();
+  turn_times(2);
+}
+
+void find_next_line() {
+  turn_left();
+  if(in_front_of_wall()) {
+    turn_right();
+    check_right();
+  } else {
+    step();
+    if(!on_ball()) {
+      turn_times(2);
+      step();
+      turn_left();
+      check_right();
+    }
+  }
+}
+
+void check_right() {
+  turn_right();
+  if(in_front_of_wall()) {
+    // Due to the assumption of only 1 side wall, we know the other side must be empty with no ball
+    // As such we can move to the empty square.
+    turn_times(2);
+    step();
+  } else {
+    step();
+  }
+}
+
+void face_north() {
+  while(!north()) {
+    turn_left();
+  }
+}
+
+void face_east() {
+  face_north();
   turn_right();
 }
