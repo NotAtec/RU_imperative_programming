00d72d9363dfe4e58bd19546bf5b9e65baef2b88
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2022-09-21 23:26:02 +0200

    Files for week 3

---
+++ week_3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/********************************************************************
+    Assignment part 2: Inclusion
+********************************************************************/
+void inclusion(double epsilon, double v)
+{
+    //implement this function
+}
+
+/********************************************************************
+    Assignment part 3: Newton-Raphson
+********************************************************************/
+void newtonraphson(double epsilon, double v)
+{
+    //implement this function
+}
+
+int main()
+{
+    return 0;
+}
