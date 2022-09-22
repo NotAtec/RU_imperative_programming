29513f962094ed9c2999a9787d1e995571e3f8c6
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2022-09-23 01:05:14 +0200

    Solve Part 4

--- week_3/main.cpp
+++ week_3/main.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 /********************************************************************
-    Desktop Test:
+    Part 1 - Desktop Test:
     Inclusion:
     Inclusion square root of 0 is 0
     Inclusion square root of 1 is 1
@@ -25,10 +25,62 @@ using namespace std;
     Newton Raphson square root of 4 is 2.0006097561 for epsilon 0.1
     Newton Raphson square root of 20 is 4.47831444547 for epsilon 0.1
 
+    Part 4 - Comparing the Algorithms:
+    Inclusion:
+    v = 0
+    0       0       0       1
+
+    v = 1
+    0       0       1       1
+    
+    v = 0.5
+    0       0       0.5     1
+    1       0.5     0.75    1
+    
+    v = 4
+    0       0       2       4
+    
+    v = 20
+    0       0       10      20
+    1       0       5       10
+    2       0       2.5     5
+    3       2.5     3.75    5
+    4       3.75    4.375   5
+    5       4.375   4.6875  5
+    6       4.375   4.53125 4.6875
+    7       4.375   4.45312 4.53125
+    8       4.45312 4.49219 4.53125
+    9       4.45312 4.47266 4.49219
+    
+    Newton Raphson:
+    v = 0
+    0       1
+    1       0.5
+    2       0.25
+    
+    v = 1
+    0       1
+    
+    v = 0.5
+    0       1
+    1       0.75
+    
+    v = 4
+    0       4
+    1       2.5
+    2       2.05
+    3       2.00061
+    
+    v = 20
+    0       20
+    1       10.5
+    2       6.20238
+    3       4.71347
+    4       4.47831
 ********************************************************************/
 
 /********************************************************************
-    Type, Variable & Function Definitions
+    Function Definitions
 ********************************************************************/
 void inclusion_output(double lower, double upper, double est, double v,
                       double eps) {
@@ -48,6 +100,7 @@ void inclusion(double epsilon, double v) {
   double x = 0.0;         // The current estimation
   double a = 0.0;         // The lower estimation bound
   double b = max(v, 1.0); // The upper estimation bound
+  int n = 0;              // Iteration
 
   // Since return values aren't allowed to be used yet, this has to be inside
   // this function due to scope. "Proper" styleguide adherence would make me
@@ -59,11 +112,17 @@ void inclusion(double epsilon, double v) {
   else
     x = (a + b) / 2;
 
+  cout << n << '\t' << a << '\t' << x << '\t' << b << endl;
   // Same principle regarding return values applies here. In case of proper
   // style-guide adherance I would separate this part of the function into it's
   // own sub-function aswell, which then returns the final value of 'x'.
   while (abs(x * x - v) > epsilon) {
     x = (a + b) / 2;
+    if(!(n==0)) 
+      cout << n << '\t' << a << '\t' << x << '\t' << b << endl;
+
+    n++;
+    
     if (x * x < v)
       a = x;
     else
@@ -78,7 +137,7 @@ void inclusion(double epsilon, double v) {
 ********************************************************************/
 void newtonraphson(double epsilon, double v) {
   double x = max(v, 1.0); // Initial Estimation
-  double n = 0;           // Iteration
+  int n = 0;              // Iteration
   cout << n << '\t' << x << endl;
 
   while (abs(x * x - v) > epsilon) {
