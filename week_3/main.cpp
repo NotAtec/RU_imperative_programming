5a06e38458bdf2de0642c425f706429c25f99115
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2022-09-22 12:21:55 +0200

    Add comment styling && Desktop Test (Part 1)

--- week_3/main.cpp
+++ week_3/main.cpp
@@ -1,6 +1,35 @@
+/********************************************************************
+    Course: Imperative Programming - Assignment 2
+    Authors: Jeroen Brinkhorst [S1101799]
+             Andrei Ujica [S1102725]
+    Date: 22.09.2022
+********************************************************************/
+
 #include <iostream>
 
 using namespace std;
+
+/********************************************************************
+    Function Prototypes
+********************************************************************/
+
+/********************************************************************
+    Desktop Test:
+    Inclusion:
+    Inclusion square root of 0 is 0
+    Inclusion square root of 1 is 1
+    Inclusion square root of 0.5 is 0.75 for epsilon 0.1
+    Inclusion square root of 4 is 2 for epsilon 0.1
+    Inclusion square root of 20 is 4.47265625 for epsilon 0.1
+
+    Newton-Raphson:
+    Newton Raphson square root of 0 is 0.25 for epsilon 0.1
+    Newton Raphson square root of 1 is 1 for epsilon 0.1
+    Newton Raphson square root of 0.5 is 0.75 for epsilon 0.1
+    Newton Raphson square root of 4 is 2.0006097561 for epsilon 0.1
+    Newton Raphson square root of 20 is 4.47831444547 for epsilon 0.1
+
+********************************************************************/
 
 /********************************************************************
     Assignment part 2: Inclusion
@@ -22,3 +51,7 @@ int main()
 {
     return 0;
 }
+
+/********************************************************************
+    Function Definitions
+********************************************************************/
