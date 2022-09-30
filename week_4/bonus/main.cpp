209af6d7abff99116a8754675bd1cf77c20997ce
Andrei Ujica <andrei.uji22@gmail.com>
2022-09-30 14:20:44 +0300

    refactor: create separate folders for bonus and mandatory

---
+++ week_4/bonus/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+enum Month
+{
+  January = 1, February, March, April, May, June,
+  July, August, September, October, November, December
+};
+
+void show_month (Month month, int year)
+{
+	// implement this function
+}
+
+void show_months ()
+{
+	// implement this function
+}
+
+#ifndef TESTING
+int main ()
+{
+    return 0;
+}
+#endif
