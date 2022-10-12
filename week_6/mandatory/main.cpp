cb4e2cad66f671a55b42eeb72f97fc9601309c54
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2022-10-12 21:47:09 +0200

    feat: Solution for part 2

--- week_6/mandatory/main.cpp
+++ week_6/mandatory/main.cpp
@@ -27,7 +27,61 @@ const int NO_OF_ROWS = 40;
 const int NO_OF_COLUMNS = 60;
 
 /********************************************************************
-    Function Definitions
+    Helper Functions
+********************************************************************/
+
+// Override the cout operator so we can print out dead and alive characters
+// easier.
+ostream &operator<<(ostream &out, Cell c) {
+  // Pre-Condition: -> Assert true is used to keep all exception handling as in
+  // the original handling.
+  assert(true);
+  // Post-condition: Regular << functionality is done, or the character for DEAD
+  // or LIVE is processed if an enum is passed.
+
+  switch (c) {
+  case Dead:
+    out << DEAD;
+    break;
+  case Live:
+    out << LIVE;
+    break;
+  }
+
+  return out;
+}
+
+Cell char_to_cell(char c) {
+  // Pre-Condition:
+  assert(c == DEAD || c == LIVE);
+  // Post-Condition: A cell enum is returned with value that is equivalent to
+  // the character given as input.
+
+  if (c == DEAD)
+    return Dead;
+
+  return Live;
+}
+
+bool process_line(Cell universe[NO_OF_ROWS][NO_OF_COLUMNS], string line,
+                  int &row, int &col) {
+  // Pre-condition:
+  assert(row >= 0 && col >= 0);
+  // Post-Condition: A line of characters is checked for correct value, and then
+  // put into the array as an enum.
+
+  for (char c : line) {
+    if (c != DEAD && c != LIVE)
+      return false;
+
+    universe[row][col] = char_to_cell(c);
+    col++;
+  }
+  return true;
+}
+
+/********************************************************************
+    Conway's Game of Life
 ********************************************************************/
 
 // Part 1: get cell in bounded universe
@@ -38,23 +92,61 @@ Cell cell_at(Cell universe[NO_OF_ROWS][NO_OF_COLUMNS], int row, int column) {
   // dead, and live if it is live. Returns Dead if out of bounds.
 
   if (row > NO_OF_ROWS || row < 0 || column > NO_OF_COLUMNS || column < 0 ||
-      (universe[row][column] != Dead && universe[row][column] != Live)) {
+      (universe[row][column] != Dead && universe[row][column] != Live))
     return Dead;
-  }
 
   return universe[row][column];
 }
 
 //  Part 2: setting the scene
-bool read_universe_file (string filename, Cell universe [NO_OF_ROWS][NO_OF_COLUMNS])
-{
-    // pre-conditions, post-conditions, implementation
+bool read_universe_file(string filename,
+                        Cell universe[NO_OF_ROWS][NO_OF_COLUMNS]) {
+  // Pre-Condition:
+  assert(true);
+  // Post-Condition: Iff no errors occur, true is returned && universe array
+  // will have loaded in universe from text-file.
+
+  ifstream config;
+  config.open(filename);
+
+  if (config.fail())
     return false;
+
+  int row = 0;
+  int col = 0;
+
+  for (string line; getline(config, line);) {
+    if (!process_line(universe, line, row, col))
+      return false;
+
+    if (col < NO_OF_COLUMNS)
+      return false;
+
+    col = 0;
+    row++;
+  }
+
+  if (row < NO_OF_ROWS)
+    return false;
+
+  return true;
 }
 
-void show_universe (Cell universe [NO_OF_ROWS][NO_OF_COLUMNS])
-{
-    // pre-conditions, post-conditions, implementation
+void show_universe(Cell universe[NO_OF_ROWS][NO_OF_COLUMNS]) {
+  // Pre-Condition:
+  assert(true);
+  // Post-Condition: Universe is printed to console.
+
+  cout << "Universe:\n\n";
+  for (int i = 0; i <= NO_OF_ROWS; i++) {
+    for (int j = 0; j <= NO_OF_COLUMNS; j++) {
+      if (universe[i][j] != Dead && universe[i][j] != Live)
+        cout << Dead;
+
+      cout << universe[i][j];
+    }
+    cout << endl;
+  }
 }
 
 //  Part 3: the next generation
