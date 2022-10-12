9e0e6e28292d34e1c8620d6a9317dc2003940452
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2022-10-12 19:59:50 +0200

    feat: Solve part 1

--- week_6/mandatory/main.cpp
+++ week_6/mandatory/main.cpp
@@ -5,9 +5,9 @@
              Andrei Ujica [S1102725]
     Date: 12.10.2022
 ********************************************************************/
-#include <iostream>
-#include <fstream>
 #include <cassert>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -15,22 +15,34 @@ using namespace std;
     Global Variable Definitions
 ********************************************************************/
 
-enum Cell {Dead=0, Live};                         // a cell is either Dead or Live (we use the fact that dead = 0 and live = 1)
+enum Cell { // a cell is either Dead or Live (we use the fact that dead = 0 and
+            // live = 1)
+  Dead = 0,
+  Live
+};
 
-const char DEAD             = '.' ;               // the presentation of a dead cell (both on file and screen)
-const char LIVE             = '*' ;               // the presentation of a live cell (both on file and screen)
-const int NO_OF_ROWS        = 40 ;                // the number of rows (height) of the universe (both on file and screen)
-const int NO_OF_COLUMNS     = 60 ;                // the number of columns (width) of the universe (both on file and screen)
+const char DEAD = '.';
+const char LIVE = '*';
+const int NO_OF_ROWS = 40;
+const int NO_OF_COLUMNS = 60;
 
 /********************************************************************
     Function Definitions
 ********************************************************************/
 
 // Part 1: get cell in bounded universe
-Cell cell_at (Cell universe [NO_OF_ROWS][NO_OF_COLUMNS], int row, int column)
-{
-    // pre-conditions, post-conditions, implementation
+Cell cell_at(Cell universe[NO_OF_ROWS][NO_OF_COLUMNS], int row, int column) {
+  // pre-condition:
+  assert(true);
+  // Post-condition: enum type Cell is returned with Dead if cell is read as
+  // dead, and live if it is live. Returns Dead if out of bounds.
+
+  if (row > NO_OF_ROWS || row < 0 || column > NO_OF_COLUMNS || column < 0 ||
+      (universe[row][column] != Dead && universe[row][column] != Live)) {
     return Dead;
+  }
+
+  return universe[row][column];
 }
 
 //  Part 2: setting the scene
