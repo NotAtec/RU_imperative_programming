ab3b253b483f7ac84f259aec195704a03762b2fd
Jeroen Brinkhorst <jeroenb04@live.com>
2022-10-21 23:18:31 +0200

    ClangD Formatting

--- week_7/mandatory/main.cpp
+++ week_7/mandatory/main.cpp
@@ -1,83 +1,88 @@
-#include <iostream>
-#include <fstream>
 #include <cassert>
 #include <cstring>
+#include <fstream>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-const int MAX_NO_OF_WORDS = 25000;       // the maximum number of words to read from a text file
-string content [MAX_NO_OF_WORDS] = {};   // the read content of a text file (at most MAX_NO_OF_WORDS words)
+const int MAX_NO_OF_WORDS = 25000;
+// the maximum number of words to read from a text file
 
-bool read_word (ifstream& infile, string& word)
-{// precondition:
-    assert (infile.is_open());
-/*  postcondition:
-    result is true only if a word is read from `infile`, and this word is assigned to `word`.
-    result is false otherwise, and the value of `word` should not be used.
-*/
-    infile >> word;
-    return !infile.fail();
+string content[MAX_NO_OF_WORDS] = {};
+// the read content of a text file (at most MAX_NO_OF_WORDS words)
+
+bool read_word(ifstream &infile, string &word) { // precondition:
+  assert(infile.is_open());
+  /*  postcondition:
+      result is true only if a word is read from `infile`, and this word is
+     assigned to `word`. result is false otherwise, and the value of `word`
+     should not be used.
+  */
+  infile >> word;
+  return !infile.fail();
 }
 
-int enter_command (string filename)
-{// precondition:
-    assert (true);
-/*  postcondition:
-    if result is a positive number then the text file with file name `filename` has been successfully opened and closed after reading, and result number of words
-    have been read from that file (at most MAX_NO_OF_WORDS) and are stored in that order in `content`.
-    if result is zero, then either the text file was empty or has not been opened.
-*/
-    int counter = 0;
+int enter_command(string filename) { // precondition:
+  assert(true);
+  /*  postcondition:
+      if result is a positive number then the text file with file name
+     `filename` has been successfully opened and closed after reading, and
+     result number of words have been read from that file (at most
+     MAX_NO_OF_WORDS) and are stored in that order in `content`. if result is
+     zero, then either the text file was empty or has not been opened.
+  */
+  int counter = 0;
 
-    ifstream infile;
-    infile.open(filename);
+  ifstream infile;
+  infile.open(filename);
 
-    if (!infile.is_open()){
-        cout << "[FileError]: Operation failed. Please check the filename.\n";
-        return 0;
-    }
+  if (!infile.is_open()) {
+    cout << "[FileError]: Operation failed. Please check the filename.\n";
+    return 0;
+  }
 
-    string word = "";
-    while(read_word(infile, word) && counter < MAX_NO_OF_WORDS){
-        content[counter] = word;
-        counter ++;
-    }
+  string word = "";
+  while (read_word(infile, word) && counter < MAX_NO_OF_WORDS) {
+    content[counter] = word;
+    counter++;
+  }
 
-    // Error code -1 used for logging error when buffer is full
-    // but did not reach EOF
-    if(read_word(infile, word))
-        return MAX_NO_OF_WORDS;
+  // Error code -1 used for logging error when buffer is full
+  // but did not reach EOF
+  if (read_word(infile, word))
+    return MAX_NO_OF_WORDS;
 
-    return counter;
+  return counter;
 }
 
-int count_command (string content [], int no_of_words, string terms [], int no_of_terms)
-{// precondition:
-    assert (no_of_words >= 0 && no_of_terms > 0);
-/*  postcondition:
-    result is the number of occurrences of the word sequence `terms` (which has `no_of_terms` elements) in `content` (which has `no_of_words` elements)
-    (the result is obviously zero in case `no_of_words` < `no_of_terms`)
-*/
-    int counter = 0;
-    bool flag = false;
+int count_command(string content[], int no_of_words, string terms[],
+                  int no_of_terms) { // precondition:
+  assert(no_of_words >= 0 && no_of_terms > 0);
+  /*  postcondition:
+      result is the number of occurrences of the word sequence `terms` (which
+     has `no_of_terms` elements) in `content` (which has `no_of_words` elements)
+      (the result is obviously zero in case `no_of_words` < `no_of_terms`)
+  */
+  int counter = 0;
+  bool flag = false;
 
-    for (int i = 0; i < no_of_words; i++){
-        if (content[i] == terms[0]){
-            flag = true;
+  for (int i = 0; i < no_of_words; i++) {
+    if (content[i] == terms[0]) {
+      flag = true;
 
-            for (int j = 1; j < no_of_terms; j++){
-                if (content[i + j] != terms[j])
-                    flag = false;
-        }
-        }
-        if (flag) {
-            counter ++;
-            flag = false;
-        }
+      for (int j = 1; j < no_of_terms; j++) {
+        if (content[i + j] != terms[j])
+          flag = false;
+      }
     }
+    if (flag) {
+      counter++;
+      flag = false;
+    }
+  }
 
-    return counter;
+  return counter;
 }
 
 void helper_count(string command, string content [], int word_count){
